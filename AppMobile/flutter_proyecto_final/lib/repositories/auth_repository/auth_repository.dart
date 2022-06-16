b36d20d72e97872dce1212d09ba93885a65139e1
DanielFB95 <fernandez.bedan20@triana.salesianos.edu>
2022-06-16 19:09:51 +0200

    Arreglando el repositorio

--- AppMobile/flutter_proyecto_final/lib/repositories/auth_repository/auth_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_proyecto_final/models/dto/login_dto.dart';
-import 'package:flutter_proyecto_final/models/login_response.dart';
-
-abstract class AuthRepository {
-  Future<LoginResponse> login(LoginDto loginDto);
-
-  //Future<SignUpResponse> signUp(SignUp signUp, String file);
-}
--- AppMobile/flutter_proyecto_final/lib/utils/constant.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-class Constant {
-  static const String URL_API_BASE =
-      "https://consultamedicaapp.herokuapp.com"; //para conectarse al local host del ordenador se utiliza la ip 10.0.2.2
-}
